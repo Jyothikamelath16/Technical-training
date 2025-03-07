0e9b50033e9a27e0d5b88478c3f19bcc9e7e4eee
Jyothikamelath16 <jyothikam16@gmail.com>
2025-03-07 15:24:39 +0530

    Create areacircumference.py

---
+++ areacircumference.py
@@ -0,0 +1,6 @@
+PI=3.1416
+r=float(input())
+circumference=2*PI*r
+print("The circumference of the circle is: {:.2f}".format(circumference))
+area=PI*r*r
+print("The area of the circle is: {:.2f}".format(area))
